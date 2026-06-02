28febb107205bd1f6b5f60cf1eb4782e132ba882
Omar Gutierrez <omargtdev@gmail.com>
2026-06-02 18:47:23 -0500

    add fish config

---
+++ fish/conf.d/myabbrs.fish
@@ -0,0 +1,17 @@
+# Tmux
+abbr --add txs ~/.config/tmux/scripts/tmux-session
+abbr --add tx-delete-all ~/.config/tmux/scripts/tmux-delete-session
+
+# K8S
+abbr --add k kubectl
+abbr --add k-contexts kubectl config get-contexts
+abbr --add k-use kubectl config use-context
+abbr --add k-current kubectl config current-context
+
+# Editor
+if command -q nvim
+    abbr --add v nvim
+else
+    abbr --add v vim
+end
+
---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+  fish_add_path /opt/homebrew/bin
+  fish_add_path ~/.local/bin
+  fish_add_path ~/.opencode/bin
+end
