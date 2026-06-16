4fe96867c47cec5a1b69d01a8f7d89d3d4c77409
Omar Gutierrez <omargtdev@gmail.com>
2026-06-16 17:40:42 -0500

    fish: onyl add rbenv if exists

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,8 @@ if status is-interactive
   fish_add_path /opt/homebrew/bin
   fish_add_path ~/.local/bin
   fish_add_path ~/.opencode/bin
-end
 
-# Added by `rbenv init` on Mon Jun  8 01:18:32 -05 2026
-status --is-interactive; and rbenv init - --no-rehash fish | source
+  if type -q rbenv
+    rbenv init - --no-rehash fish | source
+  end
+end
