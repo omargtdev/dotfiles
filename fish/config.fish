e3abdcd4c8d70d4dcaba2e03f69936e8ca8bdfd9
Omar Gutierrez <omargtdev@gmail.com>
2026-06-20 01:20:29 -0500

    fish: add new variables and fix indent

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,17 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
-  fish_add_path /opt/homebrew/bin
-  fish_add_path ~/.local/bin
-  fish_add_path ~/.opencode/bin
+    fish_add_path /opt/homebrew/bin
+    fish_add_path ~/.local/bin
+    fish_add_path ~/.opencode/bin
 
-  if type -q rbenv
-    rbenv init - --no-rehash fish | source
-  end
+    if type -q rbenv
+        rbenv init - --no-rehash fish | source
+    end
+
+    # bun
+    set --export BUN_INSTALL "$HOME/.bun"
+    set --export PATH $BUN_INSTALL/bin $PATH
+
+    set -gx OMO_SEND_ANONYMOUS_TELEMETRY 0
 end
+
